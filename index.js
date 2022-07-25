4b8426c80627c64cb5808fbba52c148f89e736b7
Marc <fischerbootcamp@gmail.com>
2022-07-25 19:19:23 +0800

    Created routes, and USer js models

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express()
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
+// const multer = require("multer");
+
+dotenv.config();
+// Created Connection
+mongoose.connect(
+    process.env.MONGODB_URI || process.env.MONGO_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+      console.log("Connected to MongoDB");
+    }
+  );
+
+//   Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+
+
+app.listen(8800, ()=>{
+    console.log("Server is running")
+})
