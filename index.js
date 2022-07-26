4b88f2377dfdf478ac5b8d2d4dce0527fcec99df
Marc <fischerbootcamp@gmail.com>
2022-07-26 21:51:17 +0800

    post

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
+const postRoute = require("./routes/posts")
 // const multer = require("multer");
 
 dotenv.config();
@@ -26,6 +27,7 @@ app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 
 
