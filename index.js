b907c8d2b5c1298165968bc3e2d2287a5855c39b
Marc <fischerbootcamp@gmail.com>
2022-08-11 18:36:42 +0800

    updated index

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
 const postRoute = require("./routes/posts")
-// const multer = require("multer");
+const multer = require("multer");
+const router = express.Router();
+const path = require("path");
 
 dotenv.config();
 // Created Connection
@@ -19,16 +21,32 @@ mongoose.connect(
       console.log("Connected to MongoDB");
     }
   );
-
+  app.use("/images", express.static(path.join(__dirname, "public/images")));
 //   Middleware
 app.use(express.json());
 app.use(helmet({
   crossOriginEmbeddedPolicy: false
 }));
-
-
 app.use(morgan("common"));
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+   
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("Image uploaded");
+  } catch (error) {
+    console.error(error);
+  }
+});
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
@@ -36,6 +54,6 @@ app.use("/api/posts", postRoute);
 
 const PORT = process.env.PORT || 8800;
 
-app.listen(prompt, ()=>{
+app.listen(PORT, ()=>{
     console.log("Server is running")
 })
